edbe483383971515940a055fe412ed31be402841
Diwakar Sharma <diwakar2034@yahoo.com>
2021-04-03 23:55:46 -0400

    "change in rgistration and security configuration"

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -3,14 +3,13 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -25,59 +24,34 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private  SSUserDetailsService userDetailsService;
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     public UserDetailsService userDetailsServiceBean() throws Exception{
         return new SSUserDetailsService(userRepository);
     }
 
+
+    @SuppressWarnings("deprecation")
     @Bean
-    public DaoAuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        authenticationProvider.setUserDetailsService(userDetailsService);
-        return authenticationProvider;
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
     }
-
-
 
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
       auth
-              .authenticationProvider(authenticationProvider());
-             // .userDetailsService(userDetailsServiceBean());
+              .userDetailsService(userDetailsServiceBean());
 
 
     }
-   /* @Bean
-    public CustomDa0uthenticationProvider daoAuthenticationProvider() {
-        CustomDaoAuthenticationProvider authenticationProvider = new CustomDaoAuthenticationProvider();
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        authenticationProvider.setUserDetailsService(userService);
-        System.out.println("Using my custom DaoAuthenticationProvider");
-        return authenticationProvider;
-    }*/
-
-
-/*
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-*/
 
 
 
-   /* @SuppressWarnings("deprecation")
-    @Bean
-    public static NoOpPasswordEncoder passwordEncoder() {
-        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
-    }*/
+
+
+
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
